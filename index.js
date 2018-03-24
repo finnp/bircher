9a7707968e915908f08b5f2f9236709cfd7b773d
Finn Pauls <derfinn@gmail.com>
2018-03-24 20:40:05 +0100

    Turn on dev tools in production

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ css('dom-notifications')
 css('./custom.css')
 
 var app = choo()
-if (process.env.NODE_ENV !== 'production') {
-  app.use(require('choo-devtools')())
-} else {
-  app.use(require('choo-service-worker')())
-}
+app.use(require('choo-devtools')())
 
 app.use(require('./stores/errors'))
 app.use(require('./stores/irc'))
